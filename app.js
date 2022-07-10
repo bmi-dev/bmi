c8a1b199e0d69f2a7df0a04531ac144d33eea2a2
Rodolfo Castillo <rcvallada@gmail.com>
2022-07-10 00:54:15 -0700

    Add app server

---
+++ app.js
@@ -0,0 +1,38 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+import express, { application } from 'express';
+import helmet from 'helmet';
+import art from 'express-art-template';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(helmet());
+
+app.engine('art', art);
+app.set('view engine', 'art');
+
+app.use(async (req, res, next) => {
+  try {
+    const packageJson = await fs.readFile(path.join(__dirname, 'package.json'), 'utf8');
+    res.locals.version = JSON.parse(packageJson).version;
+    next();
+  } catch (e) {
+    // Bad file read
+    console.error(e);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    version: res.locals.version
+  });
+});
+
+app.listen(7272);
